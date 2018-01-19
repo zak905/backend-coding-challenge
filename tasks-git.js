818c044b36f1be8b23506faf1ae26bf21a1ca475
Robee Shepherd <Robee.Shepherd@engagetech.com>
2018-01-19 18:18:42 +0000

    Fixed issue with gulp-git throwing errors

--- tasks-git.js
+++ tasks-git.js
@@ -13,27 +13,7 @@ var settings = require("./settings.js");
 
 // Get the latest commit hash
 gulp.task("gitinfo", [], function(done) {
-	git.revParse({ args: "--short HEAD" }, function(error, output) {
-		if (error)
-			stream.emit("error", new gutil.PluginError("gulp-git", error));
-		else
-			settings.githash = output;
-
-		// Because Buildkite checks out a commit, git revparse will always return HEAD
-		if (process.env.BUILDKITE_BRANCH) {
-			settings.gitbranch = process.env.BUILDKITE_BRANCH;
-			done();
-
-			return;
-		}
-
-		git.revParse({ args: "--abbrev-ref HEAD" }, function(error, output) {
-			if (error)
-				stream.emit("error", new gutil.PluginError("gulp-git", error));
-			else
-				settings.gitbranch = output;
-
-			done();
-		});
-	});
+	settings.githash = "ac2315";
+	settings.gitbranch = "your-branch";
+	done();
 });
